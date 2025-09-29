4fb7c6b094c21bba786c287401d6667b925e8304
Frocia <hikigai228@gmail.com>
2025-09-29 12:24:13 +0500

    Main.java

--- Main.java
+++ Main.java
@@ -7,16 +7,24 @@ public class Main {
 
         while (true) {
             System.out.println("\nМеню:");
-            System.out.println("1. Задание 1.1 — Точки ");
-            System.out.println("2. Задание 2.1 — Линии ");
+            System.out.println("1. Задание 1.1 — Точки");
+            System.out.println("2. Задание 2.1 — Линии");
             System.out.println("3. Задание 3.1 — Студенты");
-            System.out.println("4. Задание 4.1 — Создание точек ");
-            System.out.println("5. Задание 4.2 — Создание линий ");
-            System.out.println("6. Задание 5.3 — Длина линии ");
+            System.out.println("4. Задание 4.1 — Создание точек");
+            System.out.println("5. Задание 4.2 — Создание линий");
+            System.out.println("6. Задание 5.3 — Длина линии");
             System.out.println("0. Выход");
             System.out.print("Выберите пункт: ");
 
-            int choice = scanner.nextInt();
+            int choice;
+            if (scanner.hasNextInt()) {
+                choice = scanner.nextInt();
+            } else {
+                System.out.println("Ошибка ввода. Введите число.");
+                scanner.next();
+                continue;
+            }
+
             switch (choice) {
                 case 1: lab.task11(scanner); break;
                 case 2: lab.task21(scanner); break;
