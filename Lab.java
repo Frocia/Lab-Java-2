891e39dabdb7ba830d0ed5355f9de4cb6418238d
Frocia <hikigai228@gmail.com>
2025-09-29 12:36:53 +0500

    Lab.java

--- Lab.java
+++ Lab.java
@@ -3,81 +3,7 @@ import java.util.Scanner;
 
 public class Lab {
 
-    /** ---------------- Точка ---------------- */
-    static class Point {
-        private int x;
-        private int y;
-
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public int getX() { return x; }
-        public int getY() { return y; }
-        public void setX(int x) { this.x = x; }
-        public void setY(int y) { this.y = y; }
-
-        @Override
-        public String toString() {
-            return "{" + x + ";" + y + "}";
-        }
-    }
-
-    /** ---------------- Линия ---------------- */
-    static class Line {
-        private Point start;
-        private Point end;
-
-        public Line(Point start, Point end) {
-            this.start = start;
-            this.end = end;
-        }
-
-        public Line(int x1, int y1, int x2, int y2) {
-            this.start = new Point(x1, y1);
-            this.end = new Point(x2, y2);
-        }
-
-        public Point getStart() { return start; }
-        public Point getEnd() { return end; }
-
-        public void setStart(Point start) { this.start = start; }
-        public void setEnd(Point end) { this.end = end; }
-
-        @Override
-        public String toString() {
-            return "Линия от " + start + " до " + end;
-        }
-
-        public int getLength() {
-            int dx = end.getX() - start.getX();
-            int dy = end.getY() - start.getY();
-            return (int) Math.sqrt(dx * dx + dy * dy);
-        }
-    }
-
-    /** ---------------- Студент ---------------- */
-    static class Student {
-        private String name;
-        private int[] grades;
-
-        public Student(String name, int[] grades) {
-            this.name = name;
-            this.grades = grades;
-        }
-
-        public void setGrades(int[] grades) { this.grades = grades; }
-        public int[] getGrades() { return grades; }
-        public String getName() { return name; }
-
-        @Override
-        public String toString() {
-            return name + ": " + Arrays.toString(grades);
-        }
-    }
-
-    /** ---------------- Методы ---------------- */
+    // Метод безопасного считывания целого числа
     private int readInt(Scanner scanner, String prompt) {
         int value;
         while (true) {
@@ -87,7 +13,7 @@ public class Lab {
                 break;
             } else {
                 System.out.println("Ошибка! Введите целое число.");
-                scanner.next(); // пропустить некорректный ввод
+                scanner.next(); // пропускаем некорректный ввод
             }
         }
         return value;
@@ -127,8 +53,7 @@ public class Lab {
         line3 = new Line(line1.getStart(), line2.getEnd());
 
         System.out.println("\nПосле изменений:");
-        System.out.
-                println("Линия 1: " + line1);
+        System.out.println("Линия 1: " + line1);
         System.out.println("Линия 2: " + line2);
         System.out.println("Линия 3: " + line3);
     }
